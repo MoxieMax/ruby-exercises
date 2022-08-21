bb6e4213b1883d785875b235b8a40b2aab8b9c1c
Max MacGillivray <mary.macgillivray@gmail.com>
2022-08-21 15:21:18 -0700

    Completed hobbit.rb

--- mythical-creatures/lib/hobbit.rb
+++ mythical-creatures/lib/hobbit.rb
@@ -1,15 +1,28 @@
 class Hobbit
-  attr_reader :name, :disposition, :age
-  def initialize(name, disposition, age)
+  attr_reader :name, :disposition, :age, :has_ring
+  def initialize(name, disposition = "homebody", age=0, has_ring=false)
     @name = name
     @disposition = disposition
     @age = age
-  end
-  def adult?
-    return true if @age >= 33
-    else false
+    @has_ring = has_ring
   end
   def celebrate_birthday
     @age += 1
   end
+  def adult?
+    return true if @age > 32
+    false
+  end
+  def old?
+    true if @age >= 101
+  end
+  def has_ring?
+    return true if @name = "Frodo"
+    false
+  end
+  def is_short?
+    return true
+  end
 end
+
+#     require 'pry'; binding.pry
