84a479e2493837c4a1a941c76add33ffbb6af915
Max MacGillivray <mary.macgillivray@gmail.com>
2022-08-21 13:12:31 -0700

    Finished dragon.rb and started hobbit.rb

--- mythical-creatures/lib/dragon.rb
+++ mythical-creatures/lib/dragon.rb
@@ -1,1 +1,22 @@
-dragon
+class Dragon
+  attr_reader :name, :rider, :color, :meal_count, :hungry
+  def initialize(name, color, rider, hungry = true, meal_count=0)
+    @name = name
+    @color = color
+    @rider = rider
+    @hungry = true
+    @meal_count = 0
+  end
+  def hungry?
+    hungry
+  end
+  def eat
+    @meal_count += 1
+    # alternately written as: @hungry=false if meal_count >=3
+    if @meal_count >= 3
+      @hungry = false
+    end
+  end
+end
+
+    # require 'pry'; binding.pry
--- mythical-creatures/lib/hobbit.rb
+++ mythical-creatures/lib/hobbit.rb
@@ -1,1 +1,15 @@
-hobbit
+class Hobbit
+  attr_reader :name, :disposition, :age
+  def initialize(name, disposition, age)
+    @name = name
+    @disposition = disposition
+    @age = age
+  end
+  def adult?
+    return true if @age >= 33
+    else false
+  end
+  def celebrate_birthday
+    @age += 1
+  end
+end
